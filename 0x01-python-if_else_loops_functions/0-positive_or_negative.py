6d527e50f6001914234dadbf16c6f51a221a0c27
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-04 00:24:23 +0100

    Style fixes

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-print(f"{number:d} is", end= " ")
+print(f"{number:d} is", end=" ")
 if number > 0:
-	print("positive")
+    print("positive")
 elif number < 0:
-	print("negative")
+    print("negative")
 else:
-	print("zero")
+    print("zero")
--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -3,11 +3,11 @@ import random
 number = random.randint(-10000, 10000)
 last = int(str(number)[-1:])
 if number < 0:
-	last *= -1
-print(f"Last digit of {number} is {last} and is", end= " ")
+    last *= -1
+print(f"Last digit of {number} is {last} and is", end=" ")
 if last > 5:
-	print("greater than 5")
+    print("greater than 5")
 elif last == 0:
-	print("0")
+    print("0")
 else:
-	print("less than 6 and not 0")
+    print("less than 6 and not 0")
