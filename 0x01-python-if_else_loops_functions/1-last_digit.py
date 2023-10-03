7d109b538a39fae9795e75837eb32981be66a5d8
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-04 00:33:54 +0100

    TypeError fix

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-last = int(str(number)[-1:])
+last = abs(number) % 10
 if number < 0:
     last *= -1
 print(f"Last digit of {number:d} is {last:d} and is", end=" ")
