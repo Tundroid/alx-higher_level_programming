14c147ad975c170480ed0a1b3a98f0035fd262d1
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-04 01:02:18 +0100

    Task 5

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 for i in range(100):
-    print("{}".format(format(i, '02')), end=", ")
-print("\b\ba")
+    print("{}".format(format(i, '02')), end="")
+    if i != 99:
+        print(", ", end="")
