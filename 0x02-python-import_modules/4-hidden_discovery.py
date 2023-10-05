b32b569a5b5e77b69a796c49f6adfbc3ddd3fec8
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-05 12:51:55 +0100

    Task 4

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
     import hidden_4
-    ls = dir()
+    ls = dir(hidden_4)
     for item in ls:
         if not item.startswith("__"):
             print("{}".format(item))
