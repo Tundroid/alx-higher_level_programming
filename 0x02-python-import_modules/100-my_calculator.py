68e2e7efcc71dfeaad27123fd80e2c45c595b9f4
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-05 13:29:00 +0100

    Task 6

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    import sys, calculator_1 as calc
+    import sys
+    import calculator_1 as calc
     if len(sys.argv) != 4:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit (1)
+        exit(1)
     op = sys.argv[2]
     if op not in {'+', '-', '*', '/'}:
         print("Unknown operator. Available operators: +, -, * and /")
-        exit (1)
+        exit(1)
     a = int(sys.argv[1])
     b = int(sys.argv[3])
     res = 0
