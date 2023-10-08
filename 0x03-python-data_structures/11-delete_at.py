52113e88948c80c215c75834fcdcf1d1c81e94cc
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-08 16:11:47 +0100

    Task 12

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    new_list = []
+    for i in range(0, len(my_list)):
+        if i != idx:
+            new_list.append(my_list[i])
+    my_list = new_list
+    return new_list
