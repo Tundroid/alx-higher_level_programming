94b4dff73183d27477a7746a9c2cdb3c8483d2d6
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-08 14:32:14 +0100

    Task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python3
 def print_matrix_integer(m=[[]]):
+    col = row = 0
     for row in range(len(m)):
         for col in range(len(m[row])):
             print("{:d}".format(m[row][col]), end="")
             print("{}".format(" " if col != len(m[row]) - 1 else "\n"), end="")
+    if not len(m[0]):
+        print()
