ee787ec4d44dd13c364097d16f0ad9b012324e4d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-10 01:19:39 +0100

    Task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,27 @@
+#include "lists.h"
+
+/**
+ * is_palindrome - checks is a linked list is a palindrome
+ *
+ * @head: of linked list
+ * Return: 1 if lis tis palindrome, otherwise, 0
+*/
+int is_palindrome(listint_t **head)
+{
+	int arr[1024];
+	listint_t *tmp;
+	short i = 0, j = 0;
+
+	if (!head || !*head || !(*head)->next)
+		return (1);
+	tmp = *head;
+	arr[i++] = tmp->n;
+	arr[i++] = tmp->next->n;
+	tmp = tmp->next;
+	while ((tmp = tmp->next))
+		arr[i++] = tmp->n;
+	while (j != i)
+		if (arr[j++] != arr[--i])
+			return (0);
+	return (1);
+}
