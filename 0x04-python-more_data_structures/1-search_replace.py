3c4039aab17520b3f1969a009b574c8741bf6181
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-12 01:21:19 +0100

    Task 1

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,3 @@
+#!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    new_matrix = []
-    for row in matrix:
-        result = list(map(lambda x: x**2, row))
-        new_matrix.append(result)
-    return new_matrix
+    return [replace if elem == search else elem for elem in my_list]
