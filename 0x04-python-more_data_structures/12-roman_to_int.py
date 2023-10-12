efe308430b78d1a7f8f63295896b3b20387d9e73
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-12 03:15:57 +0100

    Task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    sum = 0
+    if roman_string and isinstance(roman_string, str):
+        rom_numerals = {'I': 1, 'V': 5, 'X': 10,
+                        'L': 50, 'C': 100, 'D': 500,
+                        'M': 1000}
+        for char in roman_string:
+            sum += rom_numerals[char]
+        return sum
