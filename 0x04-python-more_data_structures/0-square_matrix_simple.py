c953d814cd6fcf8126f80079586b8a81c53a6a93
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-12 01:14:24 +0100

    Task 0-fix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,5 +1,4 @@
 def square_matrix_simple(matrix=[]):
-    # return [[elem**2 for elem in row] for row in matrix]
     new_matrix = []
     for row in matrix:
         result = list(map(lambda x: x**2, row))
--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,4 +1,4 @@
-def square_matrix_simple(matrix=[]):
+def search_replace(my_list, search, replace):
     new_matrix = []
     for row in matrix:
         result = list(map(lambda x: x**2, row))
