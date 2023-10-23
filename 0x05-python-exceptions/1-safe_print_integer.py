1fe87e699faf07bee173adc5575c016274eb8bdc
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-23 23:05:00 +0100

    Task 1 fix

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -2,8 +2,7 @@
 def safe_print_integer(value):
     try:
         print("{:d}".format(value))
-    except ValueError:
-        print("", end="")
-        return False
-    else:
         return True
+    except (TypeError, ValueError):
+        print()
+        return False
