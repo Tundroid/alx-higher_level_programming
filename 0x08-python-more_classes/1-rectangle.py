4fb4e8f3ba02ad4195fb4d617f86eebcf5a458f9
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-31 03:18:44 +0100

    Task 1

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+"""Define a class Rectangle."""
+
+
+class Square:
+    """Represent a Rectangle."""
+    def __init__(self, width=0, height=0):
+        """Initialize a new Rectangle.
+
+        Args:
+            width (int): The width of the new rectangle.
+            height (int): The height of the new rectangle.
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Property to get width"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Property to set width"""
+        if type(value) is int:
+            if value >= 0:
+                self.__width = value
+            else:
+                raise ValueError('width must be >= 0')
+        else:
+            raise TypeError('width must be an integer')
+
+    @property
+    def height(self):
+        """Property to get height"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Property to set height"""
+        if type(value) is int:
+            if value >= 0:
+                self.__height = value
+            else:
+                raise ValueError('height must be >= 0')
+        else:
+            raise TypeError('height must be an integer')
