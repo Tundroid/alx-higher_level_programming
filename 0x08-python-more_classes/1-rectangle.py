ebc549006875eb0ae3139645e4358b6d00131cfd
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-10-31 03:20:39 +0100

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -2,7 +2,7 @@
 """Define a class Rectangle."""
 
 
-class Square:
+class Rectangle:
     """Represent a Rectangle."""
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
