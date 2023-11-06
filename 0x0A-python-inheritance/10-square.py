610eca9ea06bdcd28008233a15135ca76bca757c
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-06 17:43:06 +0100

    Task 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Defines a class Square that inherits from Rectangle."""
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """Represents a Square"""
+
+    def __init__(self, size):
+        """Initializes object
+
+        Args:
+            size (int): size of square
+        """
+        super().__init__(size, size)
