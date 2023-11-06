be3ed73179c7859b73f01b9d12590423feebf580
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-06 16:13:25 +0100

    Task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,8 +4,9 @@
 
 class MyList(list):
     """Blueprint a MyList inheriting from list."""
+    def __init__(self):
+        """initializes the object"""
+        super().__init__()
 
     def print_sorted(self):
-        tmp = self.copy()
-        tmp.sort()
-        print(tmp)
+        print(sorted(self))
