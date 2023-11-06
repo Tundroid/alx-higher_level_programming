2a527df2c9460b4d42d0ea4723c5ae2e08d2e458
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-06 17:28:21 +0100

    Task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from BaseGeometry."""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    """Represents a Rectangle"""
+
+    def __init__(self, width, height):
+        """Initializes object
+
+        Args:
+            width (int): width of rectangle
+            height (int): height of rectangle
+        """
+        super().__init__()
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__width = width
+        self.__height = height
+
+    def area(self):
+        return self.__width * self.__height
+
+    def __str__(self):
+        return f"[Rectangle] {self.__width}/{self.__height}"
