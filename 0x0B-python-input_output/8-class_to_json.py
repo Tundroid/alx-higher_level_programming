afb6b3a829d030c8a8a340b50f062ebc65818745
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-07 13:31:49 +0100

    Task 8

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Defines function that returns JSON representation of a class"""
+import json
+
+
+def class_to_json(obj):
+    """object to json string
+
+    Args:
+        obj (any) : object
+    Returns:
+        JSON representation of my_obj as string
+    """
+    return obj.__dict__
