2141ff6a4c81f377c0fa7662c09a81c13b5cf618
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-07 12:50:23 +0100

    Task 6

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Defines function to reads json text from a file"""
+import json
+
+
+def load_from_json_file(filename):
+    """reads json from a file
+
+    Args:
+        filename (str) : name of file
+    """
+    with open(filename, mode="r", encoding="utf-8") as file:
+        return json.load(file.read(filename))
