cd9a340a5b8c983c53ec10a9b16e95df292cfef8
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-13 19:20:11 +0100

    Task 6-fix

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,28 @@ class Rectangle(Base):
                                                 self.x, self.y, self.width,
                                                 self.height
                                             )
+
+    def update(self, *args, **kwargs):
+        """Update polygon"""
+        if args:
+            self.id = args[0] if len(args) > 0 else self.id
+            self.width = args[1] if len(args) > 1 else self.width
+            self.height = args[2] if len(args) > 2 else self.height
+            self.x = args[3] if len(args) > 3 else self.x
+            self.y = args[4] if len(args) > 4 else self.y
+
+        if kwargs:
+            keys = set(kwargs.keys())
+            self.id = kwargs["id"] if "id" in keys else self.id
+            self.width = kwargs["width"] if "width" in keys else self.width
+            self.height = kwargs["height"] if "height" in keys else self.height
+            self.x = kwargs["x"] if "x" in keys else self.x
+            self.y = kwargs["y"] if "y" in keys else self.y
+
+    def to_dictionary(self):
+        """Polygon to dictionary"""
+        return {'x': self.x,
+                'y': self.y,
+                'id': self.id,
+                'height': self.height,
+                'width': self.width}
