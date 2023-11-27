b74e439011b421d61173285a0eae061bf1d8af4d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2023-11-27 08:49:30 +0100

    Task 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,6 +4,17 @@
 
 class Rectangle:
     """Represent a Rectangle."""
+
+    def validate(value, name):
+        """Validator function"""
+        if type(value) is int:
+            if value >= 0:
+                return value
+            else:
+                raise ValueError('{} must be >= 0'.format(name))
+        else:
+            raise TypeError('{} must be an integer'.format(name))
+
     def __init__(self, width=0, height=0):
         """Initialize a new Rectangle.
 
@@ -11,21 +22,8 @@ class Rectangle:
             width (int): The width of the new rectangle.
             height (int): The height of the new rectangle.
         """
-        if type(width) is int:
-            if width >= 0:
-                self.__width = width
-            else:
-                raise ValueError('width must be >= 0')
-        else:
-            raise TypeError('width must be an integer')
-
-        if type(height) is int:
-            if height >= 0:
-                self.__height = height
-            else:
-                raise ValueError('height must be >= 0')
-        else:
-            raise TypeError('height must be an integer')
+        self.__width = Rectangle.validate(width, "width")
+        self.__height = Rectangle.validate(height, "height")
 
     @property
     def width(self):
@@ -35,13 +33,7 @@ class Rectangle:
     @width.setter
     def width(self, value):
         """Property to set width"""
-        if type(value) is int:
-            if value >= 0:
-                self.__width = value
-            else:
-                raise ValueError('width must be >= 0')
-        else:
-            raise TypeError('width must be an integer')
+        self.__width = Rectangle.validate(value, "width")
 
     @property
     def height(self):
@@ -51,10 +43,4 @@ class Rectangle:
     @height.setter
     def height(self, value):
         """Property to set height"""
-        if type(value) is int:
-            if value >= 0:
-                self.__height = value
-            else:
-                raise ValueError('height must be >= 0')
-        else:
-            raise TypeError('height must be an integer')
+        self.__height = Rectangle.validate(value, "height")
