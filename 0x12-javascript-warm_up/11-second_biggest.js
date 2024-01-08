652aae1da9d5a5432165f7497a2944367067a4c5
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-08 13:29:09 +0100

    Task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,8 @@
 if (process.argv.length < 4) {
   console.log(0);
 } else {
-  const arr = process.argv.slice(2);
+  let arr = process.argv.slice(2);
+  arr.sort((a, b) => b - a);
   arr.forEach(element => {
     console.log(element);
   });
