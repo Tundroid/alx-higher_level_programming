a4f95daabf872e951f42fec3977801e2494ed055
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-08 12:51:12 +0100

    Task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -3,6 +3,6 @@ if (!parseInt(process.argv[2])) {
   console.log('Missing size');
 } else {
   const size = parseInt(process.argv[2]);
-  let square = ('X'.repeat(size) + '\n').repeat(size).slice(0, -1);
+  const square = ('X'.repeat(size) + '\n').repeat(size).slice(0, -1);
   console.log(square);
 }
