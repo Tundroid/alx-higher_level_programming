6ee74d735d5271b89b3535dcae0929e45f52e3e3
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-08 10:37:24 +0100

    Task 2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,7 +1,8 @@
 #!/usr/bin/node
 const { argv } = require('node:process');
+console.log('Length:', argv.length);
 if (argv.length == 1) {
 	console.log('No argument');
 } else {
-	console.log('Argument', argv.length == 2 ? 's ' : ' ', 'found');
+	console.log('Argument', argv.length == 2 ? 's' : '', 'found');
 }
