fa70d9b4d3b3b2f79166d8106fe63f32fb1e4494
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-10 02:48:27 +0100

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,11 +1,7 @@
 #!/usr/bin/node
-const Rectangle = require('./4-rectangle');
+const BaseSquare = require('./5-square');
 
-class Square extends Rectangle {
-  constructor (size) {
-    super(size, size);
-  }
-
+class Square extends BaseSquare {
   charPrint (c) {
     if (!c) {
       super.print();
