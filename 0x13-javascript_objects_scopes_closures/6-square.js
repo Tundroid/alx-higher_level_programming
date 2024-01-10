6e23cca89a0dd4d879a2f1925998b099fcf5bcd7
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-10 02:37:07 +0100

    Task 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (!c) {
+      super.print();
+	} else {
+      const shape = (c.repeat(this.width) + '\n').repeat(this.height).slice(0, -1);
+	  console.log(shape);
+	}
+  }
+}
+module.exports = Square;
