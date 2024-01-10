6fac562b6efb4e12e364697d64aa9669134d3d5b
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-10 03:00:02 +0100

    Task 7

--- 0x13-javascript_objects_scopes_closures/7-occurrences.js
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 exports.nbOccurences = function (list, query) {
-  occurence = 0;
+  let occurence = 0;
   list.forEach(element => {
     if (element === query) {
       occurence++;
