2300ff7baf5c28678539cbb784702ea0d569619d
MOLeCULE-Soft <felixkimbu1@gmail.com>
2024-01-10 02:40:48 +0100

    Task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -9,10 +9,10 @@ class Square extends Rectangle {
   charPrint (c) {
     if (!c) {
       super.print();
-	} else {
+    } else {
       const shape = (c.repeat(this.width) + '\n').repeat(this.height).slice(0, -1);
 	  console.log(shape);
-	}
+    }
   }
 }
 module.exports = Square;
